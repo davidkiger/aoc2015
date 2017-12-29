2cf66b1c51ec5e9399d3efd12671ce649d0fc567
David Kiger <david.kiger@gmail.com>
2017-12-29 19:44:53 +0100

    Finished day 4.

---
+++ 4-2.py
@@ -0,0 +1,12 @@
+import hashlib
+m = hashlib.md5()
+
+key = 'bgvyzdsv'
+i = 0
+
+while True:
+    md5 = hashlib.md5("{}{}".format(key, i).encode('utf8')).hexdigest()
+    if md5.startswith('000000'):
+        print(i)
+        break
+    i += 1
