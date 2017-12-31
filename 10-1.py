82a8f0e784880f72ab92f4a576a4a7d6e121bc24
David Kiger <david.kiger@gmail.com>
2017-12-31 12:48:50 +0100

    Finished day 10 (with brute force).

---
+++ 10-1.py
@@ -0,0 +1,27 @@
+string = '1113122113'
+
+
+def seesay(s):
+    new_string = ''
+    digits = list(s)
+    digit = digits[0]
+    count = 1
+    for i in range(1, len(digits)):
+        if digits[i] == digit:
+            # Continuing
+            count += 1
+        else:
+            # Appending and resetting
+            new_string = '{}{}{}'.format(new_string, count, digit)
+            digit = digits[i]
+            count = 1
+    new_string = '{}{}{}'.format(new_string, count, digit)
+
+    return new_string
+
+# print(string)
+for i in range(40):
+    string = seesay(string)
+    # print(string)
+
+print(len(string))
